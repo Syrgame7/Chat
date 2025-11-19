a05fda40efbcd227db4ee51307b8e7e5e9df0e05
Syrgame7 <mahmoudmouselli389@gmail.com>
2025-11-19 20:03:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,58 +1,31 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(express.static('public'));
-
-// تخزين بيانات اللاعبين
-let players = {};
+// تقديم الملفات الثابتة (HTML, CSS, Sounds) من مجلد public
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
+    console.log('مستخدم متصل: ' + socket.id);
 
-    // إنشاء لاعب جديد عند الاتصال
-    players[socket.id] = {
-        x: 0,
-        y: 1, // ارتفاع عن الأرض
-        z: 0,
-        color: '#' + Math.floor(Math.random()*16777215).toString(16), // لون عشوائي
-        msg: ""
-    };
-
-    // إرسال اللاعبين الموجودين حالياً للاعب الجديد
-    socket.emit('currentPlayers', players);
-
-    // إبلاغ الآخرين بوجود لاعب جديد
-    socket.broadcast.emit('newPlayer', { id: socket.id, player: players[socket.id] });
-
-    // عند حركة اللاعب
-    socket.on('playerMovement', (movementData) => {
-        if(players[socket.id]) {
-            players[socket.id].x = movementData.x;
-            players[socket.id].y = movementData.y;
-            players[socket.id].z = movementData.z;
-            // إرسال الحركة للجميع
-            socket.broadcast.emit('playerMoved', { id: socket.id, x: players[socket.id].x, y: players[socket.id].y, z: players[socket.id].z });
-        }
+    // استقبال رسالة
+    socket.on('chat_message', (msgData) => {
+        // msgData = { text: "...", user: "...", time: "..." }
+        // إعادة إرسالها للجميع بما فيهم المرسل
+        io.emit('chat_message', msgData);
     });
 
-    // عند إرسال رسالة نصية
-    socket.on('chatMessage', (msg) => {
-        if(players[socket.id]) {
-            io.emit('chatMessage', { id: socket.id, msg: msg });
-        }
-    });
-
-    // عند فصل الاتصال
     socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
-        delete players[socket.id];
-        io.emit('userDisconnect', socket.id);
+        console.log('مستخدم غادر');
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
